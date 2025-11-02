7410957bcf1831ccd9ed1e28c346d3e8ad4dfdf4
BuffJesus <69093219+BuffJesus@users.noreply.github.com>
2025-11-01 20:24:36 -0600

    Added projectile, spawn projectile, basic damage effect, Rider

---
+++ Source/CrashCourse/Public/GameObjects/CC_Projectile.h
@@ -0,0 +1,35 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "CC_Projectile.generated.h"
+
+class UGameplayEffect;
+class UProjectileMovementComponent;
+
+UCLASS()
+class CRASHCOURSE_API ACC_Projectile : public AActor
+{
+	GENERATED_BODY()
+
+public:
+	
+	ACC_Projectile();
+	virtual void NotifyActorBeginOverlap(AActor* OtherActor) override;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Crash|Damage", meta = (ExposeOnSpawn, ClampMin = "0.0"))
+	float Damage{10.f};
+
+	UFUNCTION(BlueprintImplementableEvent, Category = "Crash|Projectile")
+	void SpawnImpactEffects();
+
+private:
+
+	UPROPERTY(VisibleAnywhere, Category = "Crash|Projectile")
+	TObjectPtr<UProjectileMovementComponent> ProjectileMovement;
+
+	UPROPERTY(EditDefaultsOnly, Category = "Crash|Damage")
+	TSubclassOf<UGameplayEffect> DamageEffect;
+};
