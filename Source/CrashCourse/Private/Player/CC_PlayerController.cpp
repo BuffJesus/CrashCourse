cab36a64a034766bcd477be33dcb5b243435026a
BuffJesus <69093219+BuffJesus@users.noreply.github.com>
2025-11-02 13:30:10 -0700

    Disabled player input while dying/dead. Rider

--- Source/CrashCourse/Private/Player/CC_PlayerController.cpp
+++ Source/CrashCourse/Private/Player/CC_PlayerController.cpp
@@ -7,6 +7,7 @@
 #include "AbilitySystem/CC_AbilitySystemComponent.h"
 #include "EnhancedInputComponent.h"
 #include "EnhancedInputSubsystems.h"
+#include "Characters/CC_BaseCharacter.h"
 #include "GameFramework/Character.h"
 #include "GameplayTags/CC_Tags.h"
 #include "Logging/StructuredLog.h"
@@ -40,18 +41,21 @@ void ACC_PlayerController::SetupInputComponent()
 void ACC_PlayerController::Jump()
 {
 	if (!IsValid(GetCharacter())) return;
+	if (!IsAlive()) return;
 	GetCharacter()->Jump();
 }
 
 void ACC_PlayerController::StopJumping()
 {
 	if (!IsValid(GetCharacter())) return;
+	if (!IsAlive()) return;
 	GetCharacter()->StopJumping();
 }
 
 void ACC_PlayerController::Move(const FInputActionValue& Value)
 {
 	if (!IsValid(GetPawn())) return;
+	if (!IsAlive()) return;
 	const FVector2D MovementVector = Value.Get<FVector2D>();
 
 	// Find forward direction
@@ -65,6 +69,7 @@ void ACC_PlayerController::Move(const FInputActionValue& Value)
 
 void ACC_PlayerController::Look(const FInputActionValue& Value)
 {
+	if (!IsAlive()) return;
 	const FVector2D LookAxisVector = Value.Get<FVector2D>();
 	
 	AddYawInput(LookAxisVector.X);
@@ -89,8 +94,16 @@ void ACC_PlayerController::Tertiary()
 
 void ACC_PlayerController::ActivateAbility(const FGameplayTag& AbilityTag) const
 {
+	if (!IsAlive()) return;
 	UAbilitySystemComponent* ASC = UAbilitySystemBlueprintLibrary::GetAbilitySystemComponent(GetPawn());
 	if (!IsValid(ASC)) return;
 
 	ASC->TryActivateAbilitiesByTag(AbilityTag.GetSingleTagContainer());
+}
+
+bool ACC_PlayerController::IsAlive() const
+{
+	ACC_BaseCharacter* BaseCharacter = Cast<ACC_BaseCharacter>(GetPawn());
+	if (!IsValid(BaseCharacter)) return false;
+	return BaseCharacter->IsAlive();
 }	
